946c30a9beca01e4272e776fcbf8ac9955ab8ed6
JhonRojas1997 <113646996+JhonRojas1997@users.noreply.github.com>
2025-12-11 21:30:01 -0500

    fix: resolve compiler warnings and merge conflicts

--- MeetLines.Application/DTOs/Projects/PhotoDto.cs
+++ MeetLines.Application/DTOs/Projects/PhotoDto.cs
@@ -5,7 +5,7 @@ namespace MeetLines.Application.DTOs.Projects
     public class PhotoDto
     {
         public Guid Id { get; set; }
-        public string Url { get; set; }
+        public required string Url { get; set; }
         public bool IsMain { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
     }
