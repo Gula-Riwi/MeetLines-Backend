2be49fff6e1dc1b579289be5097dd0ab1bfc5041
omar uribe <omaruribe0609@gmail.com>
2025-12-13 12:45:25 -0500

    Fix

--- MeetLines.API/DTOs/UploadProjectPhotoRequest.cs
+++ MeetLines.API/DTOs/UploadProjectPhotoRequest.cs
@@ -4,6 +4,7 @@ namespace MeetLines.API.DTOs
 {
     public class UploadProjectPhotoRequest
     {
+        [Microsoft.AspNetCore.Mvc.FromForm(Name = "file")]
         public IFormFile File { get; set; } = default!;
     }
 }
